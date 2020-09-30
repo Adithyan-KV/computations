71d52eb494da316e68b4274db55bd1535df98d25
adithyan-kv <adithyankv007@gmail.com>
2020-10-01 02:07:16 +0530

    Implemented simpsons rule
     -implemented simpsons rule for evaluating the integral
     -simpsons rule gives more accurate results with less iterations.

--- integrals/integrals.py
+++ integrals/integrals.py
@@ -1,26 +1,70 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 
 
 def main():
     lower_limit = 0
     upper_limit = 2
     function = lambda x: x**4 - 2 * x + 1
-    i = integrate_with_trapezoidal_rule(
+    I = integrate_with_trapezoidal_rule(
         function, lower_limit, upper_limit, 100)
-    print(i)
+    print(f'Integral: {I}')
+    I2 = integrate_with_simpsons_rule(function, lower_limit, upper_limit, 100)
+    print(f'Integral simpsons: {I2}')
 
 
 def integrate_with_trapezoidal_rule(function,
                                     lower_limit, upper_limit, iterations):
+    """Numerically integrates the function with the relevant limits using the
+       trapezoidal rule.
+
+    Args:
+        function (function): The function to be integrated over
+        lower_limit (float): The lower limit of integration
+        upper_limit (float): The upper limit of integration
+        iterations (int): The number of iterations for numerical computation,
+                          the higher the number, more accurate the results.
+
+
+    Returns:
+        float: The value of the evaluated integral
+    """
     h = (upper_limit - lower_limit) / iterations
-    k = np.arange(iterations)
-    # trapezoidal rule
+    # trapezoidal rule https://en.wikipedia.org/wiki/Trapezoidal_rule
     term_1 = 0.5 * function(upper_limit) + 0.5 * function(lower_limit)
     term_2 = 0
-    for k in range(1, iterations):
-        term_2 += function(lower_limit + h * k)
+    for k in range(iterations):
+        term_2 += function(lower_limit + k * h)
     I = h * (term_1 + term_2)
+    return I
+
+
+def integrate_with_simpsons_rule(function, lower_limit, upper_limit,
+                                 iterations):
+    """Numerically integrates the function with the relevant limits using the
+       Simpsons rule.
+
+    Args:
+        function (function): The function to be integrated over
+        lower_limit (float): The lower limit of integration
+        upper_limit (float): The upper limit of integration
+        iterations (int): The number of iterations for numerical computation,
+                          the higher the number, more accurate the results.
+
+
+    Returns:
+        float: The value of the evaluated integral
+    """
+    h = (upper_limit - lower_limit) / iterations
+    term_1 = (1 / 3) * h * (function(lower_limit) + function(upper_limit))
+    term_2 = 0
+    for k in range(1, iterations, 2):
+        term_2 += 4 * function(lower_limit + k * h)
+    term_3 = 0
+    for k in range(2, iterations, 2):
+        term_3 += 2 * function(lower_limit + k * h)
+    I = term_1 + h / 3 * (term_2 + term_3)
     return I
 
 
