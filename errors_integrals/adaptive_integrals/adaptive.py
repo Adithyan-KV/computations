1b5e5d9eac9445f111ea3ad9fe1f36ee5372f973
adithyan-kv <adithyankv007@gmail.com>
2020-10-06 21:32:19 +0530

    Implemented adaptive integration
     -Adaptive integration was implemented for integrating with erros less
      than a certain value.
     -Right now all the points are being recalculated for each iteration,
      can improve on this.

--- errors_integrals/adaptive_integrals/adaptive.py
+++ errors_integrals/adaptive_integrals/adaptive.py
@@ -1,9 +1,27 @@
+import math
+
+
 def main():
     f = lambda x: x**4 - 2 * x + 1
     lower_limit = 0
     upper_limit = 2
-    I = integrate_using_trapezoidal_rule(f, lower_limit, upper_limit, 10)
+    I = integrate_using_adaptive_method(f, lower_limit, upper_limit, 6)
     print(I)
+
+
+def integrate_using_adaptive_method(function, lower_limit, upper_limit, digits_precision):
+    precision_float = 1 / (10**digits_precision)
+    steps = 10
+    error = 1
+    while error > precision_float:
+        I_1 = integrate_using_trapezoidal_rule(
+            function, lower_limit, upper_limit, steps)
+        I_2 = integrate_using_trapezoidal_rule(
+            function, lower_limit, upper_limit, 2 * steps)
+        steps = steps * 2
+        error = abs((1 / 3) * (I_2 - I_1))
+    print(steps)
+    return I_2
 
 
 def integrate_using_trapezoidal_rule(function, lower_limit, upper_limit, steps):
