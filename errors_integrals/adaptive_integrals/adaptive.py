183efbe999d7785c0ce5d673f3afc716e00c8f41
adithyan-kv <adithyankv007@gmail.com>
2020-10-07 00:55:11 +0530

    Fixed major bugs, rewrote both functions
     -There was a major bug in how the counting was done in the adaptive
      function which was fixed
     -There was also a bug in the trapezoidal rule which was fixed

--- errors_integrals/adaptive_integrals/adaptive.py
+++ errors_integrals/adaptive_integrals/adaptive.py
@@ -4,52 +4,52 @@ import time
 
 def main():
     f = lambda x: x**4 - 2 * x + 1
-    lower_limit = 0
-    upper_limit = 2
-    precision_points = 6
-    start_1 = time.time()
-    I = integrate_using_adaptive_method(
-        f, lower_limit, upper_limit, precision_points)
-    end_1 = time.time()
-    I_2 = integrate_using_adaptive_trapezoidal_rule(
-        f, lower_limit, upper_limit, precision_points)
-    end_2 = time.time()
-    print(I)
+    precision_points = 10
+    I_1 = integrate_using_trapezoidal_rule(f, 0, 2, 10000)
+    print(I_1)
+    I_2 = integrate_using_adaptive_rule(f, 0, 2, precision_points)
     print(I_2)
-    print(end_1 - start_1)
-    print(end_2 - end_1)
+    I_3 = integrate_using_adaptive_rule_2(f, 0, 2, precision_points)
+    print(I_3)
 
 
-def integrate_using_adaptive_method(function, lower_limit, upper_limit, digits_precision):
-    precision_float = 1 / (10**digits_precision)
+def integrate_using_adaptive_rule(function, lower_limit, upper_limit, precision_points):
+    tolerable_error = (10)**(-precision_points)
     steps = 10
-    error = 1
-    while error > precision_float:
-        I_1 = integrate_using_trapezoidal_rule(
-            function, lower_limit, upper_limit, steps)
-        I_2 = integrate_using_trapezoidal_rule(
-            function, lower_limit, upper_limit, 2 * steps)
-        steps = steps * 2
-        error = abs((1 / 3) * (I_2 - I_1))
-    return I_2
-
-
-def integrate_using_adaptive_trapezoidal_rule(function, lower_limit, upper_limit, digits_precision):
-    tolerable_error = 1 / (10**digits_precision)
-    steps = 10
-    error = 1
     I = integrate_using_trapezoidal_rule(
         function, lower_limit, upper_limit, steps)
+    error = 1
+    # keep doubling the number of steps and get new estimates untill error is
+    # within the required levels
     while error > tolerable_error:
+        temp = I
+        steps = steps * 2
+        I = integrate_using_trapezoidal_rule(
+            function, lower_limit, upper_limit, steps)
+        error = abs(1 / 3 * (I - temp))
+    return I
+
+
+def integrate_using_adaptive_rule_2(function, lower_limit, upper_limit, precision_points):
+    # avoids the redundant calculations in the first one
+    # we already calculate half the terms of the next iteration in every
+    # iteration, that redundancy can be avoided
+    tolerable_error = (10)**(-precision_points)
+    steps = 10
+    I = integrate_using_trapezoidal_rule(
+        function, lower_limit, upper_limit, steps)
+    error = 1
+    while error > tolerable_error:
+        steps = steps * 2
         h = (upper_limit - lower_limit) / steps
+        temp = I
         additional_term = 0
-        for k in range(steps):
+        for k in range(1, steps):
             if k % 2 != 0:
                 additional_term += function(lower_limit + k * h)
-        I_new = (1 / 2) * I + h * additional_term
-        error = abs((1 / 3) * (I_new - I))
-        I = I_new
-        steps = steps * 2
+        I = (0.5 * I) + (h * additional_term)
+        error = abs((1 / 3) * (I - temp))
+        # print(error)
     return I
 
 
@@ -57,7 +57,7 @@ def integrate_using_trapezoidal_rule(function, lower_limit, upper_limit, steps):
     h = (upper_limit - lower_limit) / steps
     term_1 = (1 / 2) * (function(upper_limit) + function(lower_limit))
     term_2 = 0
-    for k in range(steps):
+    for k in range(1, steps):
         term_2 += function(lower_limit + k * h)
     I = h * (term_1 + term_2)
     return I
