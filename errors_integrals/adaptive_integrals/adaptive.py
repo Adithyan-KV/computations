3d92adfa14e32f19a43f29a1576475068bfff2d1
adithyan-kv <adithyankv007@gmail.com>
2020-10-06 23:44:37 +0530

    Implemented non redundant method for adaptive integrals
     -Previously in all the iterations all the terms were being recalculated
      this was redundant. This method was replaced.

--- errors_integrals/adaptive_integrals/adaptive.py
+++ errors_integrals/adaptive_integrals/adaptive.py
@@ -1,12 +1,18 @@
 import math
+import time
 
 
 def main():
     f = lambda x: x**4 - 2 * x + 1
     lower_limit = 0
     upper_limit = 2
-    I = integrate_using_adaptive_method(f, lower_limit, upper_limit, 6)
+    precision_points = 8
+    I = integrate_using_adaptive_method(
+        f, lower_limit, upper_limit, precision_points)
+    I_2 = integrate_using_adaptive_trapezoidal_rule(
+        f, lower_limit, upper_limit, precision_points)
     print(I)
+    print(I_2)
 
 
 def integrate_using_adaptive_method(function, lower_limit, upper_limit, digits_precision):
@@ -20,7 +26,25 @@ def integrate_using_adaptive_method(function, lower_limit, upper_limit, digits_p
             function, lower_limit, upper_limit, 2 * steps)
         steps = steps * 2
         error = abs((1 / 3) * (I_2 - I_1))
-    print(steps)
+        print(error)
+    return I_2
+
+
+def integrate_using_adaptive_trapezoidal_rule(function, lower_limit, upper_limit, digits_precision):
+    precision_float = 1 / (10**digits_precision)
+    steps = 10
+    error = 1
+    while error > precision_float:
+        h = (upper_limit - lower_limit) / steps
+        I_1 = integrate_using_trapezoidal_rule(
+            function, lower_limit, upper_limit, steps)
+        term_2 = 0
+        for k in range(steps):
+            if k % 2 != 0:
+                term_2 += function(lower_limit + k * h)
+        I_2 = (1 / 2) * I_1 + h * term_2
+        error = abs((1 / 3) * (I_2 - I_1))
+        steps = steps * 2
     return I_2
 
 
