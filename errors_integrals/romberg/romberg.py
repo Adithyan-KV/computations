3334eec7dc8fe9563e771e4589d51900dc607b3b
adithyan-kv <adithyankv007@gmail.com>
2020-10-13 22:11:14 +0530

    Implemented Romberg integration
     -A basic romberg integration was implemented

--- errors_integrals/romberg/romberg.py
+++ errors_integrals/romberg/romberg.py
@@ -1,34 +1,46 @@
 def main():
     lower_limit = 0
     upper_limit = 2
-    function = lambda x: x**4 - 2 * x + 1
-    I = integrate(function, lower_limit, upper_limit, 100)
-    print(I)
+    f = lambda x: x**4 - 2 * x + 1
+    integral = Integral(f, lower_limit, upper_limit)
+    romberg = RombergIntegral(integral)
 
 
-def integrate(function, lower_limit, upper_limit, iterations):
-    """Numerically integrates the function between the limits using trapezoidal 
-    rule
+class Integral():
 
-    Args:
-        function (function object): [the function expression to be integrated]
-        lower_limit (float): lower limit of integration
-        upper_limit (float): upper limit of integration
-        iterations (int): number of iterations over which to compute the 
-                            numerical integral. generally more iterations lead
-                            to more accurate results
+    def __init__(self, function, lower_limit, upper_limit):
+        self.function = function
+        self.lower_limit = lower_limit
+        self.upper_limit = upper_limit
 
-    Returns:
-        float: the computed value of the integral
-    """
-    h = (upper_limit - lower_limit) / iterations
-    # trapezoidal rule https://en.wikipedia.org/wiki/Trapezoidal_rule
-    term_1 = 0.5 * function(upper_limit) + 0.5 * function(lower_limit)
-    term_2 = 0
-    for k in range(1, iterations):
-        term_2 += function(lower_limit + k * h)
-    I = h * (term_1 + term_2)
-    return I
+
+class RombergIntegral():
+
+    def __init__(self, integral):
+        self.integral = integral
+        self.base_iterations = 10
+        I = self.R(3, 3)
+        print(I)
+
+    def R(self, i, m):
+        print(i)
+        if m == 1:
+            return self.integrate(self.integral, self.base_iterations * 2**(i - 1))
+        else:
+            integral_term = self.R(i, m - 1)
+            error_term = (1 / ((4**(m - 1)) - 1)) * \
+                (self.R(i, m - 1) - self.R(i - 1, m - 1))
+            return integral_term + error_term
+
+    def integrate(self, integral, iterations):
+        h = (self.integral.upper_limit - self.integral.lower_limit) / iterations
+        term_1 = 0.5 * self.integral.function(self.integral.upper_limit) + \
+            0.5 * self.integral.function(self.integral.lower_limit)
+        term_2 = 0
+        for k in range(1, iterations):
+            term_2 += self.integral.function(self.integral.lower_limit + k * h)
+        I = h * (term_1 + term_2)
+        return I
 
 
 if __name__ == "__main__":
